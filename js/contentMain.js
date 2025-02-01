8b949c056536ec0b80a45a90bf7dd7e8dd7776f0
Skimlk <vanhoutencocoapowder@gmail.com>
2025-02-01 17:37:10 -0500

    Added support for input tags

--- js/contentMain.js
+++ js/contentMain.js
@@ -3,9 +3,17 @@ import { getHost, fetchStoredString } from "./utils.js";
 async function newEvent(event) {
 	const host = await getHost();
 	const selector = await fetchStoredString(host);
-	const selectedContent = document.querySelector(selector).textContent;
+	const selectedTag = document.querySelector(selector);
+	let selectedContent;
+
+	if(selectedTag.nodeName === "INPUT") {
+		selectedContent = selectedTag.value;
+	} else {
+		selectedContent = selectedTag.innerText;
+	}
+
 	if(selectedContent == "\ufeff" || selectedContent == "")
-        browser.runtime.sendMessage({ elementClicked: true });
+		browser.runtime.sendMessage({ elementClicked: true });
 }
 function handleResponse(message, sender) {
 	document.execCommand("delete", false);
